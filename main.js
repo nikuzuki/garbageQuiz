dc5b7b85bf25e759bb9536a8d6f6a8cd08ef8d09
Kohei Makita <nikuzuki29@gmail.com>
2015-10-25 00:47:18 +0900

    add images

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 enchant();
+var cityData = ["越前町"];
+var dataArray = ["アイスノン(保冷剤)", "可燃ごみ"];
 
 window.onload = function() {
-  var core = new Game(320, 320); // 表示領域の大きさを設定
+  var core = new Game(640, 640); // 表示領域の大きさを設定
   core.fps = 24;                 // ゲームの進行スピードを設定
+  core.preload('./opendata_images/echizen.jpg');
   core.onload = function() {     // ゲームの準備が整ったらメインの処理を実行します
 
     /*
@@ -10,12 +13,22 @@ window.onload = function() {
      */
 
     var TitleScene = function() {
-      var scene = new Scene();                // 新しいシーンを作る
-      var label = new Label('福井ゴミクイズ タッチでゲームシーンへ');   // 新しいラベル(文字)を作る
+      var scene = new Scene();  // 新しいシーンを作る
+      var cityLabel = new Label('ゴミクイズ');
+      var touchLabel = new Label('TOUCH START');
 
+      cityLabel.font = "32px Palatino";
+      cityLabel.text = cityData[0] + 'ゴミクイズ';
+      cityLabel.x = 180;
+      cityLabel.y = 160;
+
+      touchLabel.font = "32px Palatino";
+      touchLabel.x = 200;
+      touchLabel.y = 480;
       // TODO: labelのタイトル文字は市町村ごとに変える
 
-      scene.addChild(label);                  // シーンにラベルに追加
+      scene.addChild(cityLabel);
+      scene.addChild(touchLabel);  // シーンにラベルに追加
 
       scene.backgroundColor = 'rgba(255, 230, 0, 1)';      // シーンの背景色を設定
       scene.addEventListener(Event.TOUCH_START, function(e) { // シーンにタッチイベントを設定
@@ -31,10 +44,18 @@ window.onload = function() {
      */
 
     var GameScene = function() {
-      var scene = new Scene();                // 新しいシーンを作る
-      var label = new Label('ゲームシーン(クイズ) タッチでゲームオーバーシーンを重ねる');        // 新しいラベル(文字)を作る
-      scene.addChild(label);                  // シーンにラベルに追加
-      scene.backgroundColor = 'rgba(255, 200, 0, 1)';      // シーンの背景色を設定
+      var scene = new Scene();  // 新しいシーンを作る
+      var label = new Label('ゲームシーン(クイズ) タッチでゲームオーバーシーンを重ねる');  // 新しいラベル(文字)を作る
+      scene.addChild(label);  // シーンにラベルに追加
+      // scene.backgroundColor = 'rgba(255, 200, 0, 1)'; // シーンの背景色を設定
+
+      var back = new Sprite(640, 640);  // 背景画像
+      back.image = core.assets['./opendata_images/echizen.jpg'];
+      // TODO: 画像はjson形式で持ってくる 街を指定すれば持ってこれる
+      back.x = 0;
+      back.y = 0;
+      core.rootScene.addChild(back);
+
 
       // TODO: 背景は各市町村の観光写真(オープンデータ)
 
