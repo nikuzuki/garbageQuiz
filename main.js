8d869cc569942ae11c368ed05804e77ad2b92949
Kohei Makita <nikuzuki29@gmail.com>
2015-10-26 16:24:27 +0900

    upgrade main.js
    選択肢用のボタンを追加した

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ enchant();
 var cityData = ["越前町"];
 var dataArray = ["アイスノン(保冷剤)", "可燃ごみ"];
 
+const WIDTH  = 640;
+const HEIGHT = 640;
+
+
 window.onload = function() {
-  var core = new Game(640, 640); // 表示領域の大きさを設定
+  var core = new Game(WIDTH, HEIGHT); // 表示領域の大きさを設定
   core.fps = 24;                 // ゲームの進行スピードを設定
   core.preload('./opendata_images/echizen.jpg');
   core.onload = function() {     // ゲームの準備が整ったらメインの処理を実行します
@@ -45,26 +49,57 @@ window.onload = function() {
 
     var GameScene = function() {
       var scene = new Scene();  // 新しいシーンを作る
-      var label = new Label('ゲームシーン(クイズ) タッチでゲームオーバーシーンを重ねる');  // 新しいラベル(文字)を作る
+      var label = new Label('ゲームシーン(クイズ) ');  // 新しいラベル(文字)を作る
       scene.addChild(label);  // シーンにラベルに追加
       // scene.backgroundColor = 'rgba(255, 200, 0, 1)'; // シーンの背景色を設定
+
 
       var back = new Sprite(640, 640);  // 背景画像
       back.image = core.assets['./opendata_images/echizen.jpg'];
 
       // TODO: 画像はjson形式で持ってくる 街を指定すれば持ってこれる
 
-
       back.x = 0;
       back.y = 0;
       core.rootScene.addChild(back);
 
+
       // TODO: 5問生成, 3問以上正解でクリア(1問あたり34%のゴミを減らす)
 
+
+      var button1 = new Button("燃えるゴミ", "dark", 25, 125);
+      button1.moveTo(160, 320);
+      scene.addChild(button1);
+      button1.ontouchstart = function(){
+        this.text = "ボタン1を押しました";
+      }
+
+      var button2 = new Button("燃えないごみ", "dark", 25, 125);
+      button2.moveTo(360, 320);
+      scene.addChild(button2);
+      button2.ontouchstart = function(){
+        this.text = "ボタン2を押しました";
+      }
+
+      var button3 = new Button("粗大ごみ", "dark", 25, 125);
+      button3.moveTo(160, 480);
+      scene.addChild(button3);
+      button3.ontouchstart = function(){
+        this.text = "ボタン3を押しました";
+      }
+
+      var button4 = new Button("回収不可", "dark", 25, 125);
+      button4.moveTo(360, 480);
+      scene.addChild(button4);
+      button4.ontouchstart = function(){
+        this.text = "ボタン4を押しました";
+      }
+      /*
       scene.addEventListener(Event.TOUCH_START, function(e) { // シーンにタッチイベントを設定
         //現在表示しているシーンの上にゲームオーバーシーンを重ねて表示します
-        core.pushScene(GameOverScene());
+        core.replaceScene(GameOverScene());
       });
+      */
       // この関数内で作ったシーンを呼び出し元に返します(return)
       return scene;
     };
